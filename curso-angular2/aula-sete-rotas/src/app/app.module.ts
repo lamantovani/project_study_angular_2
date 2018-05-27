ccd6043b0cb022d998ba2be6c87d0f86be7a8c8c
Lucas <lamantovani@gmail.com>
2018-05-27 20:06:26 -0300

    Implementação até a aula 60

--- curso-angular2/aula-sete-rotas/src/app/app.module.ts
+++ curso-angular2/aula-sete-rotas/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AlunosModule } from './alunos/alunos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -6,31 +7,36 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { routing } from './app.rounting';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+//import { CursosComponent } from './cursos/cursos.component';
+//import { routing } from './app.rounting';
+//import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
-
-
+//import { CursosService } from './cursos/cursos.service';
+//import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+import { AppRoutingModule } from './app.routing.module';
+import { CursosModule } from './cursos/curso.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    LoginComponent,//,
+    // CursosComponent,
+    // CursoDetalheComponent,
+    // CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    CursosModule,
+    AlunosModule,
+    AppRoutingModule
+    //routing
   ],
-  providers: [CursosService],
+  providers: [
+   // CursosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
